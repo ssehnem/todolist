65f2df662938ff15c2e119c43d03649890b54271
Sara Sehnem <sarasehnem84@gmail.com>
2025-07-12 01:27:04 -0300

    build: criação do banco de dados com MongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('./Models/Todo')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect()
+mongoose.connect('mongodb://127.0.0.1:27017')
 
-app.post('/add', (req, res => {
+app.post('/add', (req, res) => {
     const task = req.body.task;
-
-}))
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+app.get('/force', async (req, res) => {
+    try {
+        const result = await TodoModel.create({ task: 'Forçando criação' });
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ erro: 'Erro ao forçar criação' });
+    }
+})
 app.listen(3001, () => {
     console.log('Servidor rodando')
-    })
+})
