a1acfbe157a78005bb8a2566856cba2ba1b3baa6
ssehnem <sarasehnem84@gmail.com>
2025-07-14 10:17:50 -0300

    fix: correção pacotes adicionais

--- devops-k8s/app/todolist/vite.config.js
+++ devops-k8s/app/todolist/vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
-    }
+    },
+  build: {
+    rollupOptions: {
+      external: ['fsevents']
   }
+  }
+}
 })
