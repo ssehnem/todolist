44c2b0844750c7976b833000ea7d6abae7104d7d
sara <sarasehnem84@gmail.com>
2025-07-17 04:18:09 -0300

    build: criação nova api para puxar o backend

--- devops-k8s/app/todolist/src/Home.jsx
+++ devops-k8s/app/todolist/src/Home.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import Create from './Create'
-import axios from 'axios'
-import './App.css'
-import { BsCircleFill, BsFillTrashFill, BsFillCheckCircleFill } from 'react-icons/bs'
-import '../vite.config'
+import React, { useEffect, useState } from 'react';
+import Create from './Create';
+import axios from 'axios';
+import './App.css';
+import { BsCircleFill, BsFillTrashFill, BsFillCheckCircleFill } from 'react-icons/bs';
 
 function Home() {
+  const [todos, setTodos] = useState([]);
 
-  const [todos, setTodos] = useState([])
+  const API_URL = 'http://backend-service:3001';
 
   useEffect(() => {
-    axios.get('/api/get')
-    .then(result => setTodos(result.data))
-    .catch(err => console.log(err))
-  }, [])
+    axios.get(`${API_URL}/get`)
+      .then(result => setTodos(result.data))
+      .catch(err => console.log(err));
+  }, []);
 
   const handleEdit = (id) => {
-    axios.put('/api/update/'+id)
-    .then(result => {
-      location.reload()
-    })
-    .catch(err => console.log(err))
-  }
+    axios.put(`${API_URL}/update/${id}`)
+      .then(() => {
+        location.reload();
+      })
+      .catch(err => console.log(err));
+  };
 
   const handleDelete = (id) => {
-    axios.delete(`/api/delete/${id}`)
-    .then(() => {
-      setTodos(todos.filter(todo => todo._id !== id));
-    })
-    .catch(err => console.log(err));
-};
+    axios.delete(`${API_URL}/delete/${id}`)
+      .then(() => {
+        setTodos(todos.filter(todo => todo._id !== id));
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className="home">
@@ -38,26 +38,26 @@ function Home() {
       <Create />
       {
         todos.length === 0 
-          ? 
-          <div><h4>Nenhuma tarefa pendente</h4></div>
-          : 
-          todos.map(todo => (
-              <div className="task">
-                <div className="checkbox" onClick={() => handleEdit(todo._id)}>
-                  {todo.done ? 
-                  <BsFillCheckCircleFill className="icon"></BsFillCheckCircleFill> 
-                  : <BsCircleFill className="icon"/>
-                  }
-                  <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
-                </div>
-                <div>
-                  <span onClick={() => handleDelete(todo._id)}><BsFillTrashFill className="icon" /></span>
-                </div>
+        ? <div><h4>Nenhuma tarefa pendente</h4></div>
+        : todos.map(todo => (
+            <div className="task" key={todo._id}>
+              <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+                {todo.done 
+                  ? <BsFillCheckCircleFill className="icon" /> 
+                  : <BsCircleFill className="icon" />
+                }
+                <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
               </div>
-            ))
+              <div>
+                <span onClick={() => handleDelete(todo._id)}>
+                  <BsFillTrashFill className="icon" />
+                </span>
+              </div>
+            </div>
+          ))
       }
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
